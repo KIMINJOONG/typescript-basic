5ed3f050ed6567b12279315d057246216003fb63
KIMINJOONG <ijboym216@gmail.com>
2021-01-30 12:53:31 +0900

    init

---
+++ 1-TYPES/1-1-basic.ts
@@ -0,0 +1,32 @@
+{
+    // number
+    const num: number = -6;
+
+    // string
+    const str: string = 'hello';
+
+    // boolean
+    const bool:boolean = true;
+
+    // undefined
+    let age: number | undefined
+    age = 1;
+    age = undefined;
+
+    function find() : number | undefined {
+        return 1;
+    }
+ 
+    // null
+    let person: null // x
+    let person2: string | null;
+
+    // unknown
+    let notSure: unknown;
+    notSure = 'he';
+    notSure = true;
+
+    // any
+    let anything: any;
+
+}
