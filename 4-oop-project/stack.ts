f00106a01c5cb40552406a7ba461ee9012490745
KIMINJOONG <ijboym216@gmail.com>
2021-02-21 14:33:35 +0900

    stack

--- 4-oop-project/stack.ts
+++ 4-oop-project/stack.ts
@@ -2,31 +2,47 @@ interface Stack {
     readonly size: number;
     push(value: string): void;
     pop(): string;
-}
-
-interface List {
-    head: string | null;
-    node: string | null;
-}
-
-class ListImplement implements List {
-    head: string | null;
-    node: string | null;
-    constructor(head: string, node: string) {
-        this.head = head;
-        this.node = node;
+  }
+  
+  type StackNode = {
+    readonly value: string;
+    readonly next?: StackNode;
+  };
+  
+  class StackImpl implements Stack {
+    private _size: number = 0;
+    private head?: StackNode;
+  
+    constructor(private capacity: number) {}
+    get size() {
+      return this._size;
     }
-}
-
-class StackImplement implements Stack {
-    constructor(private list: List) {}
-    size: number;
-    push(value: string): void {
-        this.list.head = 
-        this.list.node = value;
+    push(value: string) {
+      if (this.size === this.capacity) {
+        throw new Error('Stack is full!');
+      }
+      const node: StackNode = { value, next: this.head };
+      this.head = node;
+      this._size++;
     }
     pop(): string {
-        throw new Error("Method not implemented.");
+      if (this.head == null) {
+        throw new Error('Stack is empty!');
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
     }
-
-}
+  }
+  
+  const stack = new StackImpl(10);
+  stack.push('Ellie 1');
+  stack.push('Bob 2');
+  stack.push('Steve 3');
+  while (stack.size !== 0) {
+    console.log(stack.pop());
+  }
+  
+  stack.pop();
+  
