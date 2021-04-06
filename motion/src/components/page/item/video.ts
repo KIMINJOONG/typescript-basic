a3f8def3e720e6f9a04e09020386a264a1959cf7
KIMINJOONG <ijboym216@gmail.com>
2021-04-06 17:05:17 +0900

    add video component

---
+++ motion/src/components/page/item/video.ts
@@ -0,0 +1,20 @@
+import { BaseComponent } from "../../component.js";
+
+export class VideoComponent extends BaseComponent<HTMLElement> {
+    constructor(title: string, url: string) {
+        super(`
+            <section class="video">
+                <div class="video__player">
+                    <iframe class="video__iframe"></iframe>
+                    <h3 class="video__title"></h3>
+                </div>
+            </section>
+        `);
+
+        const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
+        iframe.src = url;
+
+        const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
+        titleElement.textContent = title;
+    }
+}
