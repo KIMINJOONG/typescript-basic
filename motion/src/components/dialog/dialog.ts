dce7e825850be284e749e4a0945d4a8463998e52
KIMINJOONG <ijboym216@gmail.com>
2021-04-20 19:14:30 +0900

    fix error

--- motion/src/components/dialog/dialog.ts
+++ motion/src/components/dialog/dialog.ts
@@ -1,5 +1,5 @@
-import { Composable } from '../page/page';
-import { BaseComponent, Component } from './../component';
+import { Composable } from '../page/page.js';
+import { BaseComponent, Component } from './../component.js';
 
 type OnCloseListener = () => void;
 type OnSubmitListener = () => void;
